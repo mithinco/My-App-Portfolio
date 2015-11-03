82aaa1deebbf828553d2d97c519d1036350b71a9
mithinco <mithinco@gmail.com>
2015-11-03 00:09:34 -0800

    Add My App Portfolio

---
+++ app/src/main/java/com/example/mithinco/myapplication/MainActivity.java
@@ -0,0 +1,128 @@
+package com.example.mithinco.myapplication;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+/*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });*/
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Called when the user touches the Spotify Streamer button
+     */
+    public void clickSpotify(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Spotify Streamer app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the Scores app button
+     */
+    public void clickScores(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Scores app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the Library app button
+     */
+    public void clickLibrary(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Library app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the Build It Bigger button
+     */
+    public void clickBuild(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Build It Bigger app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the XYZ Reader button
+     */
+    public void clickXYZ(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my XYZ Reader app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    /**
+     * Called when the user touches the Capstone button
+     */
+    public void clickCapstone(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "This button will launch my Capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+}
